6ac8716778a1c834d7593ea38fbf14ad03ae5161
SIDDHARTH SRIDHAR <84910325+THETORNADOTEAM@users.noreply.github.com>
2022-02-19 22:05:48 +0530

    Update destroyx.py

--- userbot/plugins/destroyx.py
+++ userbot/plugins/destroyx.py
@@ -15,6 +15,6 @@ from telethon import events
 @borg.on(admin_cmd(pattern="destroyx ?(.*)"))
 async def _(event):
      if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
-        await event.edit("Click [here](https://github.com/CRiMiNAL786/DESTROYX) to open this 🔥**Lit AF!!**🔥 **DESTROY X** Repo.. Join Group :- @DESTROYXSUPPORT Repo Uploaded By @TornadoBot_Support")
+        await event.edit("Click [here](https://github.com/CRiMiNAL786/DESTROYX) to open this 🔥**Lit AF!!**🔥 **DESTROY X** Repo.. Join Group :- @DESTROYXSUPPORT Repo Uploaded By @MAHADEV_TORNADO_USERBOT_SUPPORT")
     
   
